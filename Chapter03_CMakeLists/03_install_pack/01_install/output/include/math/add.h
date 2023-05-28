7abc4d8b259475247ec78e20855addf831fecd23
A Shuai <ls1110924@gmail.com>
2023-05-28 16:10:47 +0800

    feat: 学习安装和打包

---
+++ Chapter03_CMakeLists/03_install_pack/01_install/output/include/math/add.h
@@ -0,0 +1,14 @@
+#ifndef SRC_C_MATH_ADD_H_
+#define SRC_C_MATH_ADD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int x, int y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ Chapter03_CMakeLists/03_install_pack/01_install/src/math/add.c
@@ -0,0 +1,5 @@
+#import "add.h"
+
+int add(int x, int y) {
+    return x + y;
+}
